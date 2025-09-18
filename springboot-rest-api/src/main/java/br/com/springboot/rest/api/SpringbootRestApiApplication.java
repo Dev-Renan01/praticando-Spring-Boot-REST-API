cc9d287806f2ce7280cb8ef3d9281bd274c04a88
Thiago Renan <thiagorenan1108@gmail.com>
2025-09-18 13:43:41 -0300

    projeto criado

---
+++ springboot-rest-api/src/main/java/br/com/springboot/rest/api/SpringbootRestApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.springboot.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Marca a classe principal da aplicação, ativando configuração automática e escaneamento de componentes
+public class SpringbootRestApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootRestApiApplication.class, args); // Linha principal que roda o projeto Java Spring Boot
+	}
+
+}
